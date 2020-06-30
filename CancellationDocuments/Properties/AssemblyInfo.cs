03d7e636c49ffb4a93e784e0328552394b84a902
blatsabidze <blatsabidze@AD.gtex.ge>
2020-06-30 15:42:51 +0400

    assembly info

--- CancellationDocuments/Properties/AssemblyInfo.cs
+++ CancellationDocuments/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("B1Studio.VSIntegration.UICSharpTemplate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DocCancellation")]
+[assembly: AssemblyDescription("RSM Document Cancellation Addon")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("B1Studio.VSIntegration.UICSharpTemplate")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("RSM")]
+[assembly: AssemblyProduct("Document Cancellation Addon")]
+[assembly: AssemblyCopyright("Copyright © RSM Georgia Solutiuons 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
